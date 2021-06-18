71fc2e476ed36f810deda3214a4035b972cd4adb
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-06-18 10:37:57 -0500

    albums seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
-
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist  VARCHAR(50),
     name    VARCHAR(50) NOT NULL,
-    release_date   DATE,
-    sales   FLOAT,
+    release_date   INT,
+    sales   DECIMAL(15,2),
     genre   VARCHAR(100),
 
     PRIMARY KEY (id)
